b6dc629fc9ba90381346b86945c558584e3a1021
shawn's macbook <shawnscivally@shawn-ski.local>
2019-05-02 15:23:23 -0600

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
+
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      baseUrl: 'https://practiceapi.devmountain.com/api'
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+  axios
+
   componentDidMount() {
-
+    axios.get(`${this.state.baseUrl}/posts`)
+      .then(res => {
+        this.setState({ posts: res.data });
+      });
   }
 
-  updatePost() {
-  
+  updatePost( id, text ) {
+    axios.put(`${this.state.baseUrl}id=${ id }`, { text }).then( results => {
+      this.setState({ posts: results.data });
+    });
   }
 
   deletePost() {
@@ -44,7 +53,17 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
-          
+          {
+            posts.map(post => (
+              <Post
+                key={post.id}
+                text={post.text}
+                date={post.date}
+                id={post.id}
+                updatePostFn={this.updatePost} />
+            ))
+          }
+
         </section>
       </div>
     );
